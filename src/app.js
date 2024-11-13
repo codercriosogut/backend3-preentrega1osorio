067cf5a0540897b7fe08c5c178e8238ad8cba620
Cristian <cosoriogut@gmail.com>
2024-11-13 00:27:06 -0300

    version2 faker dotenv

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
-
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+//add
+import mocksRouter from './routes/mocks.router.js';
 
 const app = express();
 const PORT = process.env.PORT||8080;
+//add ruta
 const connection = mongoose.connect(`mongodb+srv://cri2024:cri2024@cluster0.mswsapd.mongodb.net/bkd3_mocks?retryWrites=true&w=majority&appName=Cluster0`)
 
 app.use(express.json());
@@ -18,5 +20,7 @@ app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
 app.use('/api/adoptions',adoptionsRouter);
 app.use('/api/sessions',sessionsRouter);
+//add
+app.use('/api/mocks',mocksRouter);
 
 app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
